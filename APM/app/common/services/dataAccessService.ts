e655c3f21d9966516f62507d4abfd8031adfec27
jortiz <josepho011235@gmail.com>
2015-12-27 17:54:41 -0500

    created dat acccess service class

--- APM/app/common/services/dataAccessService.ts
+++ APM/app/common/services/dataAccessService.ts
@@ -1,10 +1,21 @@
 module app.common {
     interface IDataAccessService {
-        
     }
     
     interface IProductResource
         extends ng.resource.IResource<app.domain.IProduct> {
-        
     }
+    
+    export class DataAccessService
+        implements IDataAccessService {
+            
+            static $inject = ["$resource"];
+            constructor(private $resource: ng.resource.IResourceService) {
+                
+            }
+            
+            getProductResrouce(): ng.resource.IResourceClass<IProductResource> {
+                return this.$resource("/api/products/productId");
+            }
+        }
 }
